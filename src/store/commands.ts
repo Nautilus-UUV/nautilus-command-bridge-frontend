f3b4072763340c06ec552cccfa18cbef83266c9c
Jannick <jannick.schroer@gmail.com>
2025-05-07 11:08:04 +0200

    Faster Command Reads

--- src/store/commands.ts
+++ src/store/commands.ts
@@ -151,7 +151,7 @@ export const useCommandStore = defineStore('commands', () => {
     insertCommands(data)
   }
 
-  setInterval(checkForNew, 50000)
+  setInterval(checkForNew, 5000)
 
   return {
     commands,
